453606dd7172bc24a75948e6b69c01f068202c96
Damien353 <10034@holbertonstudents.com>
2025-03-25 10:57:07 +0100

    suppression comm

--- ES6_basic/0-constants.js
+++ ES6_basic/0-constants.js
@@ -1,18 +1,15 @@
-// Modifie taskFirst pour utiliser const
 export function taskFirst() {
-    const task = 'I prefer const when I can.'; // Utilisation de const car la variable ne change pas
+    const task = 'I prefer const when I can.';
     return task;
   }
   
-  // Fonction qui renvoie une partie du texte
   export function getLast() {
     return ' is okay';
   }
   
-  // Modifie taskNext pour utiliser let
   export function taskNext() {
-    let combination = 'But sometimes let'; // Utilisation de let car la variable va être modifiée
-    combination += getLast(); // On ajoute une autre valeur à combination
+    let combination = 'But sometimes let';
+    combination += getLast();
   
     return combination;
   }
