4a34aa55370f64559c3a3e0fbb49c1fad402d8f2
Damien353 <10034@holbertonstudents.com>
2025-04-02 15:17:12 +0200

    Return promise avec message logué

---
+++ ES6_promise/2-then.js
@@ -0,0 +1,13 @@
+export default function handleResponseFromAPI(promise) {
+  promise
+    .then(() => {
+      // Si la Promise est résolue, on retourne l'objet avec le statut et le message
+      console.log('Got a response from the API');
+      return { status: 200, body: 'success' };
+    })
+    .catch(() => {
+      // Si la Promise est rejetée, on retourne une erreur vide
+      console.log('Got a response from the API');
+      return new Error();
+    });
+}
