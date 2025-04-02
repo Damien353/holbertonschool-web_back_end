33846235509176c1557b6694cf10fa135a4ad28a
Damien353 <10034@holbertonstudents.com>
2025-04-02 15:38:56 +0200

    Return promises

---
+++ ES6_promise/3-all.js
@@ -0,0 +1,14 @@
+import { uploadPhoto, createUser } from './utils';
+
+export default function handleProfileSignup() {
+  // Exécution des deux Promises en parallèle avec Promise.all()
+  Promise.all([uploadPhoto(), createUser()])
+    .then(([photoResponse, userResponse]) => {
+      // Affichage des informations extraites des résultats des Promises
+      console.log(photoResponse.body, userResponse.firstName, userResponse.lastName);
+    })
+    .catch(() => {
+      // Si une erreur survient affiche ce message
+      console.log('Signup system offline');
+    });
+}
