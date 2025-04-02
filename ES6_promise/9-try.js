7d4221887d2b11d128fc76693479207ae04f0660
Damien353 <10034@holbertonstudents.com>
2025-04-02 17:14:04 +0200

    test modification error queue push

--- ES6_promise/9-try.js
+++ ES6_promise/9-try.js
@@ -5,7 +5,7 @@ export default function guardrail(mathFunction) {
     const result = mathFunction(); // Exécution de mathFunction
     queue.push(result); // Ajout du résultat à la queue si pas d'erreur
   } catch (error) {
-    queue.push(error.message); // Ajout du message d'erreur si une exception est levée
+    queue.push(`Error: ${error.message}`); // Ajout du message d'erreur si une exception est levée
   }
 
   queue.push('Guardrail was processed'); // Ajout du message final dans tous les cas
