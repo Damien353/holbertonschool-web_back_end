441e2c7c691139fdb0badc05b7eb29549cc2eb8f
Damien353 <10034@holbertonstudents.com>
2025-04-02 14:57:11 +0200

    correction du lint

--- ES6_promise/0-promise.js
+++ ES6_promise/0-promise.js
@@ -1,6 +1,5 @@
 export default function getResponseFromAPI() {
-  return new Promise((resolve, reject) => {
-
+  return new Promise((resolve) => {
     setTimeout(() => {
       resolve("Réponse de l'API reçue !");
     }, 2000);
