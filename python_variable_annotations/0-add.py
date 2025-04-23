57ac859bd529e2f8a7a74902d5ba5210743e68e5
Damien353 <10034@holbertonstudents.com>
2025-04-23 15:38:25 +0200

    Ajout des tasks 0 à 9 et code task 0 et 1

---
+++ python_variable_annotations/0-add.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+Module 0-add
+This module defines a function that adds two floats.
+"""
+
+def add(a: float, b: float) -> float:
+    """
+    Add two float numbers and return the result.
+
+    Args:
+        a (float): First number
+        b (float): Second number
+
+    Returns:
+        float: The sum of a and b
+    """
+    return a + b
