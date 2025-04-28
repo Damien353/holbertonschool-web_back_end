f9a2511f9369a5acbf32fad4cb9495a34a2f1c93
Damien353 <10034@holbertonstudents.com>
2025-04-28 16:22:08 +0200

    modif "next page" pour pycodestyle

--- pagination/2-hypermedia_pagination.py
+++ pagination/2-hypermedia_pagination.py
@@ -82,7 +82,9 @@ class Server:
             "page_size": len(data),
             "page": page,
             "data": data,
-            "next_page": page + 1 if (page * page_size) < dataset_length else None,
+            "next_page": (
+                page + 1 if (page * page_size) < dataset_length else None
+            ),
             "prev_page": page - 1 if page > 1 else None,
             "total_pages": total_pages
         }
