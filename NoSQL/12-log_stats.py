08bac218de59ccbdb2cd048dc6ca8a689d32c1d7
Damien353 <10034@holbertonstudents.com>
2025-04-30 11:38:42 +0200

    Log stats

---
+++ NoSQL/12-log_stats.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+from pymongo import MongoClient
+
+def log_stats():
+    client = MongoClient('mongodb://127.0.0.1:27017')
+    db = client.logs
+    collection = db.nginx
+
+    # Total number of logs
+    total_logs = collection.count_documents({})
+
+    # Count the occurrences of each method
+    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    method_counts = {}
+    for method in methods:
+        method_counts[method] = collection.count_documents({"method": method})
+
+    # Count the number of logs with method = GET and path = /status
+    status_check = collection.count_documents({"method": "GET", "path": "/status"})
+
+    # Output the results
+    print(f"{total_logs} logs")
+    print("Methods:")
+    for method in methods:
+        print(f"\tmethod {method}: {method_counts[method]}")
+    print(f"{status_check} status check")
+
+if __name__ == "__main__":
+    log_stats()
