7b1a3a7d086ad9fe693b7ce05397ccf116384147
Damien353 <10034@holbertonstudents.com>
2025-05-21 14:50:49 +0200

    test modif process stdin

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,9 +1,10 @@
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-process.stdin.on('data', (data) => {
-  const name = data.toString().trim();
+process.stdin.on('readable', () => {
+  const name = process.stdin.read();
+  if (name) {
   process.stdout.write(`Your name is: ${name}\n`);
-});
+}});
 
 // Détecter quand l'entrée standard est fermée
 process.stdin.on('end', () => {
