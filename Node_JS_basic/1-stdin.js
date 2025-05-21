4ab424672216815622b4cd13b100132c4aafd179
Damien353 <10034@holbertonstudents.com>
2025-05-21 15:02:14 +0200

    ESlint

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -3,8 +3,9 @@ process.stdout.write('Welcome to Holberton School, what is your name?\n');
 process.stdin.on('readable', () => {
   const name = process.stdin.read();
   if (name) {
-  process.stdout.write(`Your name is: ${name}`);
-}});
+    process.stdout.write(`Your name is: ${name}`);
+  }
+});
 
 // Détecter quand l'entrée standard est fermée
 process.stdin.on('end', () => {
