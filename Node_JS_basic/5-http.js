eecc96eb5a71b6233f33c34a1848ccd3019e56fc
Damien353 <10034@holbertonstudents.com>
2025-05-22 11:18:10 +0200

    HTTP server list students

---
+++ Node_JS_basic/5-http.js
@@ -0,0 +1,28 @@
+const http = require('http');
+const countStudents = require('./3-read_file_async');
+
+const DB = process.argv[2];
+
+const app = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('content-type', 'text/plain');
+
+  if (req.url === '/') {
+    res.end('Hello Holberton School!');
+  } else if (req.url === '/students') {
+    countStudents(DB)
+    .then((output) => {
+      res.end(`This is the list of our students\n${output}`);
+    })
+    .catch((err) => {
+      res.end(`This is the list of our students\n${err.message}`);
+    });
+  } else {
+    res.statusCode = 404;
+    res.end('Not Found');
+  }
+});
+
+app.listen(1245);
+
+module.exports = app;
